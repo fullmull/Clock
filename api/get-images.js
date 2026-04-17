9990e4725dd5287b5a366d1fe88417fdc31ad2c8
meow <111137222+fullmull@users.noreply.github.com>
2026-04-17 02:52:47 +0200

    Update get-images.js

--- api/get-images.js
+++ api/get-images.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+
     try {
         const { orientation = 'vertical' } = req.query;
         
